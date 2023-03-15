1b9807a31281f4ea34ec1390387ed706d3d4a60a
Monteleev <cs04421@uoi.gr>
2023-03-15 20:17:22 +0200

    Added simple front page

--- DataVis/src/main/java/com/example/datavis/controller/CountryController.java
+++ DataVis/src/main/java/com/example/datavis/controller/CountryController.java
@@ -1,9 +1,11 @@
 package com.example.datavis.controller;
 
 import com.example.datavis.dao.CountryRepository;
+import com.example.datavis.dao.StatisticsRepository;
 import com.example.datavis.entity.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Controller
-@RequestMapping(path = "/countries")
 public class CountryController {
     @Autowired
     private CountryRepository countryRepository;
-    @GetMapping
-    public @ResponseBody
-    Iterable<Country> getAllUsers() {
+
+    @Autowired
+    private StatisticsRepository statisticsRepository;
+
+    @GetMapping("/")
+    public String showData(Model model) {
         // This returns a JSON or XML with the users
-        return countryRepository.findAll();
+        model.addAttribute("countries",countryRepository.findAll());
+
+        model.addAttribute("stats",statisticsRepository.findAll());
+
+        return "home";
     }
 }
