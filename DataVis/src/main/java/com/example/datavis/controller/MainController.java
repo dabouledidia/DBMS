1b8c0b69a91ca122562c6b3a0cf2fe4e8cac9416
dabouledidia <dimitrisdim8900@gmail.com>
2023-04-03 21:36:33 +0300

    Bar Chart for one country. Not fully functional yet. Updated pom.

--- DataVis/src/main/java/com/example/datavis/controller/MainController.java
+++ DataVis/src/main/java/com/example/datavis/controller/MainController.java
@@ -6,11 +6,16 @@ import com.example.datavis.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
+import java.util.*;
+
+import org.json.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -42,15 +47,42 @@ public class MainController {
         return "query";
     }
 
+//    @PostMapping("/query")
+//    public String submitForm(@ModelAttribute("option") Options option, Statistics statistics, Model model) {
+//        System.out.println(option.getStartYear());
+//        System.out.println(option.getEndYear());
+//        List<Statistics> results = statisticsRepository.findByCodeInAndIndicatorAndYearGreaterThanAndYearLessThan
+//                (option.getCountry(),option.getIndicator(), option.getStartYear(),option.getEndYear());
+//        System.out.println(results.size());
+//        model.addAttribute("results",results);
+//        return "charts";
+//    }
+
+
     @PostMapping("/query")
-    public String submitForm(@ModelAttribute("option") Options option,Model model) {
-        System.out.println(option.getStartYear());
-        System.out.println(option.getEndYear());
-        List<Statistics> results = statisticsRepository.findByCodeInAndIndicatorAndYearGreaterThanAndYearLessThan
-                (option.getCountry(),option.getIndicator(), option.getStartYear(),option.getEndYear());
-        System.out.println(results.get(93).getValue());
-        System.out.println(results.size());
-        model.addAttribute("results",results);
-        return "charts";
+    public ModelAndView chartForm(@ModelAttribute("option") Options option, Statistics statistics, ModelMap model) {
+        int start = option.getStartYear();
+        List<Statistics> stats = statisticsRepository.findByCodeInAndIndicatorAndYearGreaterThanAndYearLessThan(option.getCountry(),option.getIndicator(), option.getStartYear(),option.getEndYear());
+        System.out.println(stats.size());
+        model.addAttribute("stats",stats);
+        Map<Integer, Double> valuesAndYear= new HashMap<Integer, Double>();
+        for (int i = 0; i < stats.size(); i++){
+            valuesAndYear.put((i+start), stats.get(i).getValue());
+        }
+
+        List<Integer> sortYear = new ArrayList<Integer>(valuesAndYear.keySet());
+        Collections.sort(sortYear);
+        String s = "key, value\n";
+        for (Integer i : sortYear){
+            Double v = valuesAndYear.get(i);
+            s = s + i + ", " + v + "\n";
+        }
+
+        String pageTitle = "Bar Chart";
+
+        JSONArray results = CDL.toJSONArray(s);
+        model.addAttribute("dataMap", results);
+        model.addAttribute("title", pageTitle);
+        return new ModelAndView("barchart", model);
     }
 }
